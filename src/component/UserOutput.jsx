020926452fb4dcd8d5fe905358bf5f789afe591a
Cecilia Muyide <83959507+ceccy-ope@users.noreply.github.com>
2023-06-14 15:15:00 +0100

    Update UserOutput.jsx

--- src/component/UserOutput.jsx
+++ src/component/UserOutput.jsx
@@ -1,20 +1,11 @@
 import React from 'react'
 
-export default function UserOutput() {
+export default function UserOutput({username }) {
     //creating a  UserOutput element
   return (
     <div>
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.
-            Alias quae nam dolore at, inventore, eaque vero voluptate 
-            reiciendis praesentium fugiat, tenetur possimus et accusamus? 
-            Eligendi quis totam ab quae quisquam.
-        </p><br></br><br />
-        <p>
-            Lorem ipsum dolor, sit amet consectetur adipisicing elit. 
-            Eveniet molestias quod repudiandae doloremque possimus recusandae ipsa unde eos? 
-            Omnis cupiditate explicabo animi non dolorum natus nihil doloribus, 
-            quis repellendus voluptates.
-        </p>
+        <p>{username} is the most gentle boy in class</p>
     </div>
   )
 }
+
